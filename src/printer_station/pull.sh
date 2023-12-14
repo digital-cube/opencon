7f551f68569b3d52d9dfb7d1c884193239e6e1e4
Igor Jeremic <igor@digitalcube.rs>
2023-12-14 19:35:12 +0100

    Removing printer-station

--- src/printer_station/pull.sh
+++
@@ -1,2 +0,0 @@
-cd /home/pi/2023/sfscon/src/printer_station
-GIT_SSH_COMMAND="ssh -i ~/2023/id_rsa" git pull
--- src/printer_station/server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/pi/2023/sfscon/src/printer_station
-
-.venv/bin/python server.py
